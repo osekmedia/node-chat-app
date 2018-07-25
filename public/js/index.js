3799176eab4396abc92351e4f6747f589ec056d1
osekmedia <osekmedia@gmail.com>
2018-07-24 20:32:36 -0700

    Adding mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -11,28 +11,31 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
 
-	console.log('New message', message);
-
 	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery("#message-template").html();
 
-	var li = jQuery('<li></li>');
+	var html = Mustache.render( template, {
+		text: message.text,
+		from: message.from,
+		createdAt: formattedTime
+	});
 
-	li.text(`${message.from} ${formattedTime}: ${message.text}`);
+	jQuery('#messages').append(html);
 
-	jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
 
 	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery("#location-message-template").html();
 
-	console.log('New location message', message);
-	var li = jQuery('<li></li>');
-	var a = jQuery('<a target="_blank">My current location</a>');
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href', message.url );
-	li.append(a);
-	jQuery('#messages').append(li);
+	var html = Mustache.render( template, {
+		url: message.url,
+		from: message.from,
+		createdAt: formattedTime
+	});
+
+	jQuery('#messages').append(html);
 
 });
 
